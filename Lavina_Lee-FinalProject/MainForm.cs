93a053d7b720786a77fb976f24d60ee340262884
thanielLee <thaniel.lee@student.ateneo.edu>
2025-04-19 20:52:01 +0800

    Main Form added

---
+++ Lavina_Lee-FinalProject/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lavina_Lee_FinalProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm(User current_user)
+        {
+            InitializeComponent();
+        }
+    }
+}
